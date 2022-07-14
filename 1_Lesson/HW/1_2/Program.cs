966d026386e38f9ab5277f95a5e33aad38c36bc2
Vadamoff <Vadamoff@mail.ru>
2022-07-14 13:29:17 +0300

    HW 1_2

---
+++ 1_Lesson/HW/1_2/Program.cs
@@ -0,0 +1,33 @@
+﻿//4
+//Напишите программу,
+//которая принимает на вход три числа
+//и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите 1 число:");
+string a = Console.ReadLine();
+
+Console.WriteLine("Введите 2 число:");
+string b = Console.ReadLine();
+
+Console.WriteLine("Введите 3 число:");
+string c = Console.ReadLine();
+
+int num_a = int.Parse(a);
+int num_b = int.Parse(b);
+int num_c = int.Parse(c);
+int large = num_a;
+
+if(num_b > num_a)
+    {
+        large = num_b;
+        if(num_c > num_b)
+        {
+            large = num_c;
+        }
+    }
+else if(num_c > num_a)
+    {
+        large = num_c;
+    }
+
+Console.WriteLine($"Число {large} - самое большое");
