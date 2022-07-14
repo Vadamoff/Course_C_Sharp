8b94f54123179f3ad46d00f90a0a3af443ee501c
Vadamoff <Vadamoff@mail.ru>
2022-07-14 13:33:00 +0300

    HW 1_3

---
+++ 1_Lesson/HW/1_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//6
+//Напишите программу,
+//которая на вход принимает число и выдаёт,
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число:");
+string a = Console.ReadLine();
+
+int num = int.Parse(a);
+
+if(num % 2 == 0)
+    {
+        Console.WriteLine($"Число {num} - чётное");
+    }
+else
+    {
+        Console.WriteLine($"Число {num} - нечётное");
+    }
