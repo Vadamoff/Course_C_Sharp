e03039de58b4980c65886fd61b1a7a14b768c417
Vadamoff <Vadamoff@mail.ru>
2022-07-14 12:19:01 +0300

    1_2

---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//1. Напишите программу, которая на вход принимает два числа
+//и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите первое число:");
+string s_a = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+string s_b = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if(a == b * b)
+{
+    Console.WriteLine($"{a} является квадратом {b}");
+}
+else
+{
+    Console.WriteLine($"{a} не является квадратом {b}");
+}
