01cddfb1af9784080f8624b55a6256a7e3e1fa70
Vadamoff <Vadamoff@mail.ru>
2022-07-14 13:40:44 +0300

    HW 1_5

---
+++ 1_Lesson/HW/1_5/Program.cs
@@ -0,0 +1,13 @@
+﻿//7
+//Напишите программу,
+//которая принимает на вход трёхзначное число
+//и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Введите число:");
+string n = Console.ReadLine();
+
+int N = int.Parse(n);
+
+int Remain = N % 10;
+
+Console.WriteLine(Remain);
