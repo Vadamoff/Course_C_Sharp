1ec5c066d7f080c4f94d6dcc970f088e390b9c49
Vadamoff <Vadamoff@mail.ru>
2022-07-18 21:21:56 +0300

    HW 2_3

---
+++ 2_Lesson/HW/2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//3
+//Напишите программу,
+//которая выводит третью цифру заданного числа
+//или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine());
+
+if(n < 100)
+    Console.WriteLine("третьей цифры нет");
+else
+{
+    while(n >= 1000)
+    {
+        n = n / 10;
+    }
+    Console.WriteLine(n % 10);
+}
