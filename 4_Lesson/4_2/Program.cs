266900048beeb5797d7525f1d9fcc7727ecc960a
Vadamoff <Vadamoff@mail.ru>
2022-08-08 17:04:38 +0300

    4_2

---
+++ 4_Lesson/4_2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 1:
+//Напишите программу,
+//которая принимает на вход число
+//и выдаёт количество цифр в числе.
+
+void Length(int a)
+{
+    string num = a.ToString();
+    Console.WriteLine(num.Length);
+}
+
+Length(1670);
