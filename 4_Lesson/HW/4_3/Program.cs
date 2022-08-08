457b96989fe800da0438cfa891e3a9f1c6937cd4
Vadamoff <Vadamoff@mail.ru>
2022-08-08 22:49:33 +0300

    HW 4_3

---
+++ 4_Lesson/HW/4_3/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 3:
+//Напишите программу,
+//которая задаёт массив из 8 элементов случайными числами
+//и выводит их на экран.
+//Оформите заполнение массива и вывод в виде функции.
+
+void FillArray(int[] arr, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        arr[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int leng = arr.Length;
+    for(int i = 0; i < leng; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+Console.WriteLine("Введите целое число:");
+int num = int.Parse(Console.ReadLine());
+
+int[] array = new int[num];
+
+FillArray(array, num);
+
+Console.WriteLine($"Массив из {num} элементов:");
+PrintArray(array);
