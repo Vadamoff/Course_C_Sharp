7c5852002a336d5a17b44958235868626275e4ff
Vadamoff <Vadamoff@mail.ru>
2022-08-10 15:26:10 +0300

    5_2

---
+++ 5_Lesson/5_2/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 1:
+//Напишите программу замена элементов массива:
+//положительные элементы
+//замените на соответствующие отрицательные,
+//и наоборот.
+
+int[] CreateArray(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void InvertSymbol(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = - arr[i];
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    int leng = arr.Length;
+    for(int i = 0; i < leng; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = CreateArray(12, -9, 10);
+
+PrintArray(array);
+
+InvertSymbol(array);
+
+PrintArray(array);
