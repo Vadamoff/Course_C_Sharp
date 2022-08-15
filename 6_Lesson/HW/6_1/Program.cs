78407ff9f8d9b3625bd25637a864c7f8c56fb432
Vadamoff <Vadamoff@mail.ru>
2022-08-15 18:23:46 +0300

    HW 6_1

---
+++ 6_Lesson/HW/6_1/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 1:
+//Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] CreateArray()
+{
+    Console.WriteLine("Введите количество чисел:");
+    int num = int.Parse(Console.ReadLine());
+
+    int[] arr = new int[num];
+    
+    for(int i = 0; i < num; i++)
+    {
+        Console.WriteLine("Введите число:");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int SumPos(int[] arr)
+{
+    int sum = 0;
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+            sum += 1;
+    }
+
+    return sum;
+}
+
+int[] array = CreateArray();
+
+PrintArray(array);
+
+Console.WriteLine($"Вы ввели {SumPos(array)} чисел больше 0");
