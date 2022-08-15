2c53af7fdb118e1e825c5f23d9a126e7a1e0a726
Vadamoff <Vadamoff@mail.ru>
2022-08-15 17:49:14 +0300

    6_4

---
+++ 6_Lesson/6_4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 3:
+//Не используя рекурсию,
+//выведите первые N чисел Фибоначчи.
+//Первые два числа Фибоначчи: 0 и 1.
+
+void Fibonacci(int num)
+{
+    int a = 0;
+    int b = 1;
+    for(int i = 0; i < num; i++)
+    {
+        Console.Write($"{a} ");
+        (a, b) = (b, a + b);
+    }
+}
+
+Console.WriteLine("Введите целое положительное число:");
+int N = int.Parse(Console.ReadLine());
+
+Fibonacci(N);
