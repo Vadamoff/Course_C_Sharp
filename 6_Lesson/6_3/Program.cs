ad6a68caf182979b6ee3389e06f40d358ac79026
Vadamoff <Vadamoff@mail.ru>
2022-08-15 17:34:40 +0300

    6_3

---
+++ 6_Lesson/6_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 2:
+//Напишите программу,
+//которая будет преобразовывать десятичное число в двоичное.
+
+string DecToBin(int n)
+{
+    string result = "";
+    while(n > 0)
+        {
+            result = (n % 2) + result;
+            n /= 2;
+        }
+    return result;
+}
+
+Console.WriteLine("Введите целое число:");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{num} в десятичной системе равно {DecToBin(num)} в двоичной");
