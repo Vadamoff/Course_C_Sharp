653fb0d4d403a370016e197e84536dc87039a10e
Vadamoff <Vadamoff@mail.ru>
2022-08-16 10:46:33 +0300

    7_1

---
+++ 7_Lesson/7_1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 1:
+//Задайте двумерный массив размером m×n,
+//заполненный случайными целыми числами.
+
+int[,] CreateArray2D()
+{
+    Console.WriteLine("Задайте количество строк в массиве:");
+    int row = int.Parse(Console.ReadLine());
+    Console.WriteLine("Задайте количество столбцов в массиве:");
+    int col = int.Parse(Console.ReadLine());
+    Console.WriteLine("Задайте начало диапазона случайных чисел для элементов в массиве:");
+    int from = int.Parse(Console.ReadLine());
+    Console.WriteLine("Задайте конец диапазона случайных чисел для элементов в массиве:");
+    int to = int.Parse(Console.ReadLine());
+
+    int[,] arr = new int[row, col];
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            arr[i, j] = new Random().Next(from, to + 1);
+        }
+    }
+    
+    return arr;
+}
+
+void PrintArray2D(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] array = CreateArray2D();
+
+PrintArray2D(array);
