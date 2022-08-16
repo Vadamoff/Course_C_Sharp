ba9ef5a1b031a891fec0c5a6892758bb3611c6af
Vadamoff <Vadamoff@mail.ru>
2022-08-16 17:05:19 +0300

    7_3

---
+++ 7_Lesson/7_3/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача 2:
+//Задайте двумерный массив.
+//Найдите элементы,
+//у которых обе позиции чётные,
+//и замените эти элементы на их квадраты.
+
+int[,] CreateArray2D()
+{
+    Console.WriteLine("Задайте количество строк в массиве:");
+    int row = int.Parse(Console.ReadLine());
+    Console.WriteLine("Задайте количество столбцов в массиве:");
+    int col = int.Parse(Console.ReadLine());
+    Console.WriteLine("Задайте начало диапазона случайных чисел для элементов в массиве:");
+    int from = int.Parse(Console.ReadLine());
+    Console.WriteLine("Задайте конец диапазона случайных чисел для элементов в массиве:");
+    int to = int.Parse(Console.ReadLine());
+
+    int[,] arr = new int[row, col];
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            arr[i, j] = new Random().Next(from, to + 1);
+        }
+    }
+    
+    return arr;
+}
+
+void PrintArray2D(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] ModifyEvenPosArray2D(int[,] arr)
+{
+    for(int i = 1; i < arr.GetLength(0); i += 2)
+    {
+        for(int j = 1; j < arr.GetLength(1); j += 2)
+        {
+            arr[i, j] *= arr[i, j];
+        }
+    }
+    
+    return arr;
+}
+
+int[,] array = CreateArray2D();
+
+PrintArray2D(array);
+
+PrintArray2D(ModifyEvenPosArray2D(array));
