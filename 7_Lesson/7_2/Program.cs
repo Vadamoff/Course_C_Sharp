e21d766e6a4439ddb07ea10fad03046c216e7fce
Vadamoff <Vadamoff@mail.ru>
2022-08-16 11:36:36 +0300

    7_2

---
+++ 7_Lesson/7_2/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 1:
+//Задайте двумерный массив размера m на n,
+//каждый элемент в массиве находится по формуле: Aₙₙ = m + n.
+//Выведите полученный массив на экран.
+
+int[,] CreateArray2D()
+{
+    Console.WriteLine("Задайте количество строк в массиве:");
+    int row = int.Parse(Console.ReadLine());
+    Console.WriteLine("Задайте количество столбцов в массиве:");
+    int col = int.Parse(Console.ReadLine());
+    
+    int[,] arr = new int[row, col];
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+    
+    return arr;
+}
+
+void PrintArray2D(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] array = CreateArray2D();
+
+PrintArray2D(array);
