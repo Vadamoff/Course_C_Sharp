13d71db08817aea0d8b84198d40920ae974ab6d3
Vadamoff <Vadamoff@mail.ru>
2022-08-19 21:36:51 +0300

    9_1

---
+++ 9_Lesson/9_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 1:
+Задайте значение N.
+Напишите программу,
+которая выведет все натуральные числа
+в промежутке от 1 до N при помощи рекурсии.
+*/
+
+void ValueSet(int num)
+{
+    if(num == 0)
+        return;
+    ValueSet(num - 1);
+    Console.Write($"{num}, ");
+}
+
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine());
+
+ValueSet(N);
