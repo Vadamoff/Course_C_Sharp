6ba69049e2b0290ffebb359a762a4934fac03009
Vadamoff <Vadamoff@mail.ru>
2022-08-20 09:18:57 +0300

    9_Lesson/

---
+++ 9_Lesson/HW/9_1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 1:
+Задайте значения M и N.
+Напишите программу,
+которая выведет все чётные натуральные числа
+в промежутке от M до N с помощью рекурсии.
+*/
+
+void ValueEvenMinMax(int min, int max)
+{
+    if(min > max)
+        return;
+    if(max % 2 != 0)
+        max = max - 1;
+    ValueEvenMinMax(min, max - 2);
+    Console.Write($"{max}, ");
+}
+
+Console.WriteLine("Введите число M:");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine($"Чётные натуральные числа от {M} до {N}:");
+ValueEvenMinMax(M, N);
