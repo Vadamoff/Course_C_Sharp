62da6504a8427d0f27427e6bd1f1d40b4144ef0b
Vadamoff <Vadamoff@mail.ru>
2022-08-20 09:34:03 +0300

    HW 9_2

---
+++ 9_Lesson/HW/9_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 2:
+Задайте значения M и N.
+Напишите программу,
+которая найдёт сумму натуральных элементов
+в промежутке от M до N с помощью рекурсии.
+*/
+
+int SumElement(int min, int max)
+{
+    if(min > max)
+        return 0;
+    return SumElement(min, max - 1) + max;
+}
+
+Console.WriteLine("Введите число M:");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine($"Сумма натуральных элементов от {M} до {N}:");
+Console.WriteLine(SumElement(M, N));
